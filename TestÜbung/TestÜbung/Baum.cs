44295a82a7c0d97756cc72c25038ad33eeff3312
Nico8784 <nico.hausharter@gmail.com>
2023-02-08 18:11:15 +0100

    Abgabe
    Abgabe Nico Hausharter

---
+++ TestÜbung/TestÜbung/Baum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestÜbung
+{
+    internal abstract class Baum
+    {
+        public int hoehe { get; set; }
+        public int Stammdurchmesser { get; set; }
+
+        private DateTime setzDatum;
+
+        public DateTime SetzDatum
+        {
+            get { return setzDatum; }
+            protected set 
+            {
+                if (value<=DateTime.Now)
+                {
+                    setzDatum = value;
+                }
+            }
+        }
+        public void Heizen()
+        {
+            Console.WriteLine("Der Baum brennt gut!");
+        }
+        public abstract void Wachsen();
+    }
+    
+}
